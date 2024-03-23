b50b3702741896b6a09cb5db5b9a8bde4fa3fce4
Nikhil Prasad <nhprasad94@gmail.com>
2024-03-23 13:19:14 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1 className='text-red-500'>Nikhil Prasad's New Portfolio</h1>
+      <h1 className='text-red-500'>Nikhil Prasad's</h1>
     </div>
   );
 }
